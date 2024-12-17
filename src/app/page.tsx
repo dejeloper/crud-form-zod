1d38141d6cbdfa81bc093615db78a74782eab763
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-12-16 22:19:44 -0500

    feat: Add form with react-hook-form, zod, shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,185 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const userSchema = z.object({
+  name: z
+    .string({ required_error: "El nombre es requerido" })
+    .min(3, "El nombre debe tener al menos 3 caracteres"),
+  surname: z
+    .string({ required_error: "El apellido es requerido" })
+    .min(3, "El apellido debe tener al menos 3 caracteres"),
+  age: z
+    .string({ required_error: "La edad es requerida" })
+    .min(1, "La edad debe ser tener al menos 1 digito")
+    .max(3, "La edad debe ser tener máximo 3 digitos")
+    .transform((value) => parseInt(value)),
+  civilStatus: z
+    .string({
+      required_error: "El estado civil es requerido",
+      invalid_type_error: "Seleccione un estado civil válido",
+    })
+    .refine((value) => civilStatusOptions.includes(value), {
+      message: "Seleccione un estado civil válido",
+    }),
+});
+
+const civilStatusOptions = [
+  "Soltero",
+  "Casado",
+  "Viudo",
+  "Divorciado",
+  "Separado",
+];
+
+type UserType = z.infer<typeof userSchema>;
+
+export default function Page() {
+  const form = useForm<UserType>({
+    resolver: zodResolver(userSchema),
+    defaultValues: {
+      name: "",
+      surname: "",
+      age: 0,
+      civilStatus: "",
+    },
+  });
+
+  const onSubmit = form.handleSubmit(async (data: UserType) => {
+    console.log(data);
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <Card>
+      <CardHeader>
+        <CardTitle>Crear un nuevo Usuario</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form className="flex flex-col gap-y-4 min-w-72" onSubmit={onSubmit}>
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="name">Nombre</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      id="name"
+                      autoComplete="name"
+                    />
+                  </FormControl>
+                  {form.formState.errors.name && (
+                    <FormDescription className="text-red-500">
+                      {form.formState.errors?.name.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <FormField
+              name="surname"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="surname">Apellido</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      id="surname"
+                      autoComplete="additional-name"
+                    />
+                  </FormControl>
+                  {form.formState.errors.surname && (
+                    <FormDescription className="text-red-500">
+                      {form.formState.errors?.surname.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="age"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="age">Edad</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="number"
+                      id="age"
+                      autoComplete="age"
+                    />
+                  </FormControl>
+                  {form.formState.errors.age && (
+                    <FormDescription className="text-red-500">
+                      {form.formState.errors?.age.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="civilStatus"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem id="civilStatusFormItem">
+                  <FormLabel htmlFor="civilStatus">Civil Status</FormLabel>
+                  <Select
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                    name={field.name}
+                  >
+                    <FormControl id="civilStatusFormControl">
+                      <SelectTrigger id="civilStatus" name={field.name}>
+                        <SelectValue placeholder="Seleccione un estado civil" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent id="civilStatusFormContent">
+                      {civilStatusOptions.map((status) => (
+                        <SelectItem key={status} value={status}>
+                          {status}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  {form.formState.errors.civilStatus && (
+                    <FormDescription className="text-red-500">
+                      {form.formState.errors?.civilStatus.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Guardar</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
